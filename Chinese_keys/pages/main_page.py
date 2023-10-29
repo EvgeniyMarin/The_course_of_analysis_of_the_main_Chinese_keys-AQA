2d10f233a9257b5b5ebfeb1a821b01b14160d5ba
EvgeniyMarin <evgeniymarin7@mail.ru>
2023-10-29 13:13:30 +0300

    Тест авторизации

--- Chinese_keys/pages/main_page.py
+++ Chinese_keys/pages/main_page.py
@@ -3,7 +3,6 @@ from pages.locators import MainPageLocators
 from pages.locators import LoginFormLocators
 
 
-
 class MainPage(Base_Page):
     def the_presence_of_a_button(self):  # метод для проверки наличия кнопки "войти"
         assert self.browser.find_element(*MainPageLocators.LOGIN_BUTTON), "Кнопка ВОЙТИ не найдена"
@@ -12,7 +11,33 @@ class MainPage(Base_Page):
     def go_to_the_login_form(self):  # метод для перехода к форме логина
         button = self.browser.find_element(*MainPageLocators.LOGIN_BUTTON)
         button.click()
+        print("Передох на форму логина")
 
-    def availability_of_a_login_form(self):         # метод для проверки наличия формы логина
+    def availability_of_a_login_form(self):  # метод для проверки наличия формы логина
         assert self.browser.find_element(*LoginFormLocators.FORM_LOGIN), "Форма логина ОТСУТСТВУЕТ"
         print("Форма логина присутствует")
+
+    def the_presence_of_an_email_field(self):  # метод для проверки наличия поля ввода логина (почты)
+        assert self.browser.find_element(*LoginFormLocators.LOGIN_INPUT), "Поле ввода логина отсутствует"
+        print("Поле ввода логина присутствует")
+
+    def the_presence_of_an_password_field(self):  # метод для проверки наличия поля ввода пароля
+        assert self.browser.find_element(*LoginFormLocators.PASSWORD_INPUT), "Поле ввода пароля отсутствует"
+        print("Поле ввода пароля присутствует")
+
+    def button_log_in(self):  # метод проверки наличия кнопки для входа в систему
+        assert self.browser.find_element(*LoginFormLocators.SUBMIT_BUTTON), "Кнопка входа в систему ОТСУТСТВУЕТ"
+        print("Кнопка входа в систему ПРИСУТСТВУЕТ")
+
+    def login_input(self, login):  # метод ввода логина
+        input_email = self.browser.find_element(*LoginFormLocators.LOGIN_INPUT)
+        input_email.send_keys(login)
+
+    def password_input(self, password):  # метод ввода пароля
+        input_password = self.browser.find_element(*LoginFormLocators.PASSWORD_INPUT)
+        input_password.send_keys(password)
+
+    def log_in(self):  # метод входа в систему
+        button_log_in = self.browser.find_element(*LoginFormLocators.SUBMIT_BUTTON)
+        button_log_in.click()
+
--- Chinese_keys/test_checking_authorization.py
+++ Chinese_keys/test_checking_authorization.py
@@ -1,9 +1,35 @@
 from pages.main_page import MainPage
+import time
 
 
 def test_the_presence_of_a_login_button(browser):
     """Тест наличия входа в систему"""
     url = "https://stepik.org/catalog"
-    page = MainPage(browser, url)     # инициализация объекта Page Object
-    page.open_page()                  # открытие страницы по заданному url
+    page = MainPage(browser, url)  # инициализация объекта Page Object
+    page.open_page()  # открытие страницы по заданному url
     page.the_presence_of_a_button()
+
+
+def test_availability_of_a_login_form(browser):
+    """Тест наличия формы логина"""
+    url = "https://stepik.org/catalog"
+    page = MainPage(browser, url)  # инициализация объекта Page Object
+    page.open_page()  # открытие страницы по заданному url
+    page.go_to_the_login_form()
+    time.sleep(10)
+    page.availability_of_a_login_form()
+
+
+"""Сюда написать тесты наличия форм ввода и кнопки входа в систему"""
+
+def test_authorization_on_the_site(browser):
+    """Тест авторизации на сайте"""
+    url = "https://stepik.org/catalog?auth=login"
+    login = "evgeniymarin7@mail.ru"
+    password = "D19675d77777d"
+    page = MainPage(browser, url)
+    page.open_page()
+    page.login_input(login)
+    page.password_input(password)
+    page.log_in()
+    time.sleep(15)
